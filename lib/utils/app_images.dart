c4607bb8805a8252769b66217d879c52a8989515
amira_nasser <amira20nasser@gmail.com>
2025-10-21 12:39:31 +0300

    Add SVG assets and update app_images.dart for asset management
    - Added new SVG assets: arrow_down.svg, dashboard.svg, department.svg, employee.svg, menu.svg, message.svg, notification.svg, recruitment.svg, schedule.svg, settings.svg, support.svg, union.svg.
    - Added a PNG asset: image.png.
    - Created app_images.dart to manage asset paths for easy access.
    - Updated pubspec.yaml to include asset paths and configuration for app_images.dart.

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,57 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesArrowDown
+  /// assets/images/arrow_down.svg
+  static const String imagesArrowDown = "assets/images/arrow_down.svg";
+
+  /// Assets for imagesDashboard
+  /// assets/images/dashboard.svg
+  static const String imagesDashboard = "assets/images/dashboard.svg";
+
+  /// Assets for imagesDepartment
+  /// assets/images/department.svg
+  static const String imagesDepartment = "assets/images/department.svg";
+
+  /// Assets for imagesEmployee
+  /// assets/images/employee.svg
+  static const String imagesEmployee = "assets/images/employee.svg";
+
+  /// Assets for imagesImage
+  /// assets/images/image.png
+  static const String imagesImage = "assets/images/image.png";
+
+  /// Assets for imagesMenu
+  /// assets/images/menu.svg
+  static const String imagesMenu = "assets/images/menu.svg";
+
+  /// Assets for imagesMessage
+  /// assets/images/message.svg
+  static const String imagesMessage = "assets/images/message.svg";
+
+  /// Assets for imagesNotification
+  /// assets/images/notification.svg
+  static const String imagesNotification = "assets/images/notification.svg";
+
+  /// Assets for imagesRecruitment
+  /// assets/images/recruitment.svg
+  static const String imagesRecruitment = "assets/images/recruitment.svg";
+
+  /// Assets for imagesSchedule
+  /// assets/images/schedule.svg
+  static const String imagesSchedule = "assets/images/schedule.svg";
+
+  /// Assets for imagesSettings
+  /// assets/images/settings.svg
+  static const String imagesSettings = "assets/images/settings.svg";
+
+  /// Assets for imagesSupport
+  /// assets/images/support.svg
+  static const String imagesSupport = "assets/images/support.svg";
+
+  /// Assets for imagesUnion
+  /// assets/images/union.svg
+  static const String imagesUnion = "assets/images/union.svg";
+}
+
